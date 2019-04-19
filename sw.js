527a5e620c36e53aa1f9b4b26274f40955a89fd4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-19 06:49:09 +0000

    Deploy jolylai/docs to github.com/jolylai/docs.git:gh-pages

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-5841d259228487cf5579.js"
+    "url": "webpack-runtime-bdec9036b6922c6aff56.js"
   },
   {
     "url": "app-ae243b41d70199a08571.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "69383a3bedc978cf8ac5439144605919"
+    "revision": "3bbfa5da9d162cf022f0fc09c869a489"
   },
   {
     "url": "styles.d443cf6b45bf40ad3e0e.css"
